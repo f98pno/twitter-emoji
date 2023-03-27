b3823dc8f999b3112108730787e634c03f665835
Peter Novak <peter.novak.m@gmail.com>
2023-03-27 23:33:08 +0800

    Adding files for displaying posts and user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,9 +11,11 @@ import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
+// Using react-hook-form might have been a cleaner way to do the input validation since that is then done on the client side
 const CreatePostWizard = () => {
   const { user } = useUser();
 
@@ -97,10 +99,14 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span>{`@${author.username}`}</span>
-          <span className="font-thin">{` * ${dayjs(
-            post.createdAt
-          ).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">{` * ${dayjs(
+              post.createdAt
+            ).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-xl">{post.content}</span>
       </div>

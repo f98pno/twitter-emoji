1f10cf832e0d2856759969300a7feea16bf64a56
Peter Novak <peter.novak.m@gmail.com>
2023-03-28 11:14:46 +0800

    Using TRPCs createProxySSGHelpers for getting username to [slug] page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,16 @@ import { Toaster } from "react-hot-toast";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Twitter Emoji</title>
+        <meta name="description" content="Tweet your favourite emoji!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
